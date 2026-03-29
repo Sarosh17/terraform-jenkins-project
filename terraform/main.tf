157afa5cf55a54e7e9432a0877987b511e031fe6
SAROSH AHMED KHAN <89131155+Sarosh17@users.noreply.github.com>
2026-03-29 10:58:34 +0530

    Update security group ID for devops server

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "devops_server" {
   ami                    = "ami-0ddfba243cbee3768"
   instance_type          = "t3.micro"
   key_name               = "devops-key"
-  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  vpc_security_group_ids = [aws_security_group.devops-server-sg.id]
 
   tags = {
     Name = "Terraform-Jenkins-Server"
